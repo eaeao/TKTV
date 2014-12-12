4e4cd7cc52f264c436a5dfa3d02ca0c8e08e42dc
JerryPark <eaeao@naver.com>
2014-12-13 07:39:17 +0900

    init

---
+++ tktv/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from tktv import settings
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'tktv.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+)
+urlpatterns += staticfiles_urlpatterns()
